ae0e67820376e2cfca39a14c38dbaaba39c13879
endaulster <105703311+endaulster@users.noreply.github.com>
2023-10-15 18:57:38 -0300

    Added form functionality and data saving

--- brain.js
+++ brain.js
@@ -9,8 +9,31 @@ function Book(a, b, c, d) {
 
 function addBookToLibrary(a, b, c , d) {
     myLibrary.push(new Book(a, b, c , d));
-    return console.log("Done!");
 }
+
+// READING FORMS CONTENT
+
+function getInfo(){
+    let mAuthor = document.getElementById("authorName").value;
+    let mBook = document.getElementById("bookName").value;
+    let mPages = document.getElementById("pagesNumber").value;
+    let mStatus = statusOf;
+    if (mStatus == false) {
+        mStatus = "Not Read";
+    } else if (mStatus == true) {
+        mStatus = "Read";
+    }
+    addBookToLibrary(mAuthor, mBook, mPages, mStatus);
+    console.log(myLibrary);
+    
+}
+
+let theForm = document.getElementById("theForm");
+
+theForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    theForm.reset();
+})
 
 // MODAL OPENNING AND CLOSING
 
@@ -22,4 +45,26 @@ function openModal() {
 
 function closeModal() {
     modal.classList.remove("sub-bop");
+
+}
+
+// READ STATUS
+
+let statusButton = document.getElementById("inputs-status");
+let statusOf = false;
+
+function changeStatus(){
+
+    if (statusOf == false )
+    {
+        statusButton.style.backgroundColor = "rgb(97 203 97)";
+        statusButton.innerHTML = "Read";
+        statusOf = true;
+    } else if (statusOf == true) {
+        statusButton.style.backgroundColor = "rgb(255, 137, 137)";
+        statusButton.innerHTML = "Not Read";
+        statusOf = false;
+    }
+
+    
 }
